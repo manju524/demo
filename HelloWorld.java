6d67bdddf0ed4742b17c3c5aa079b3349d05426a
manju524 <manjunath.k.aws@gmail.com>
2018-08-07 09:15:15 +0000

    updated

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+import java.*;
+
+class HelloWorld
+{
+   public static void main(String args[]) throws Exception
+   {
+      /* public static InetAddress getLocalHost()
+       * throws UnknownHostException: Returns the address 
+       * of the local host. This is achieved by retrieving 
+       * the name of the host from the system, then resolving 
+       * that name into an InetAddress. Note: The resolved 
+       * address may be cached for a short period of time.
+       */
+    /* public String getHostAddress(): Returns the IP 
+       * address string in textual presentation.
+       */
+      System.out.println("My Helloworld");
+  }
+}
